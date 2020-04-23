7838e197f6770b3b3023266d551ba1cde250033e
tanjuha <tanya.oleksandrivna19.07@gmail.com>
2020-04-23 23:33:33 +0300

    changed language => ukraine

--- dist/js/data-table.js
+++ dist/js/data-table.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   let table = $('#example').DataTable( {
-    // dom: 'Blfrtip',
-    // "lengthMenu": [ 5, 10, 25, 50, 75, 100 ],
-    lengthChange: false,
-    buttons: [ 'copy', 'excel', 'pdf', 'colvis' ]
+    dom: 'Blfrtip',
+    "lengthMenu": [ 5, 10, 25, 50, 75, 100 ],
+    buttons: [ 'copy', 'excel', 'pdf', 'colvis' ],
+    language: {
+      "url": "https://cdn.datatables.net/plug-ins/1.10.20/i18n/Ukrainian.json"
+  }
   });
 
   table.buttons().container().appendTo( '#example_wrapper .col-md-6:eq(0)' );
